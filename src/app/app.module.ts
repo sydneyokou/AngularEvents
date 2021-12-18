3a93cea47eab3752a0ff37356770fa7d2d6e3e8b
sydneyokou <45752338+sydneyokou@users.noreply.github.com>
2021-12-18 17:45:14 +0100

    organizing exports with Barrels / index files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 
+import {
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventService,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator
+} from "./events/index";
 import { EventsAppComponent } from "./events-app.component";
-import { EventsListComponent } from "./events/events-list.component";
-import { EventThumbnailComponent } from "./events/event-thumbnail.component";
 import { NavBarComponent } from "./nav/navbar.component";
-import { EventService } from "./events/shared/event.service";
 import { ToastrService } from "./common/toastr.service";
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { appRoutes } from "./routes";
-import { CreateEventComponent } from "./events/create-event.component";
 import { Error404Component } from "./errors/404.component";
-import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
 
 export function checkDirtyState(component: CreateEventComponent) {
   if (component.isDirty) {
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from "@angular/router";
-import { EventsListComponent } from "./events/events-list.component";
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
-import { CreateEventComponent } from "./events/create-event.component";
+
+import {
+  EventsListComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventRouteActivator
+} from "./events/index";
 import { Error404Component } from "./errors/404.component";
-import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
 
 export const appRoutes: Routes = [
   { path: "events", component: EventsListComponent },
