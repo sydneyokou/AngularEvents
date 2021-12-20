7a1139181743dde47a409878c2073129204d3aaa
sydneyokou <45752338+sydneyokou@users.noreply.github.com>
2021-12-20 09:55:52 +0100

    Template-based form with : angular FormsModule, ngSubmit, ngModel

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { RouterModule } from "@angular/router";
+import { FormsModule } from "@angular/forms";
+
 import { userRoutes } from "./user.routes";
 import { ProfileComponent } from "./profile.component";
+import { LoginComponent } from "./login.component";
 
 @NgModule({
-  imports: [CommonModule, RouterModule.forChild(userRoutes)],
-  declarations: [ProfileComponent],
+  imports: [CommonModule, RouterModule.forChild(userRoutes), FormsModule],
+  declarations: [ProfileComponent, LoginComponent],
   providers: []
 })
 export class UserModule {}
